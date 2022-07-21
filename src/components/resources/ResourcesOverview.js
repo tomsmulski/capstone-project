aa279b7185b18da0dd4175f4eb0135e8a9ba6b61
Thomas Smulski <info@makenci.de>
2022-07-21 21:42:02 +0200

    change image directory path

--- src/components/resources/ResourcesOverview.js
+++ src/components/resources/ResourcesOverview.js
@@ -1,25 +1,12 @@
 import styled from 'styled-components';
 
-import rescources_icon_money from '../../images/icons/icon_money.jpg';
-import rescources_icon_iron from '../../images/icons/icon_iron.jpg';
-import rescources_icon_fuel from '../../images/icons/icon_fuel.jpg';
-import rescources_icon_gold from '../../images/icons/icon_gold.jpg';
-import rescources_icon_energy from '../../images/icons/icon_energy.jpg';
-
 export const ResourcesOverview = ({currentResource}) => {
-  const IconArray = [
-    rescources_icon_money,
-    rescources_icon_iron,
-    rescources_icon_fuel,
-    rescources_icon_gold,
-    rescources_icon_energy,
-  ];
 
   return (
     <StyledRescourceContainer>
-      {currentResource.map((currentRes, index) => (
+      {currentResource.map((currentRes) => (
         <StyledRescourceSection key={currentRes.id}>
-          <StyledRescourceIcon alt={currentRes.name} src={IconArray[index]}></StyledRescourceIcon>
+          <StyledRescourceIcon alt={currentRes.name} src={'../images/icons/icon_' + currentRes.name.charAt(0).toLowerCase() + currentRes.name.slice(1) + '.jpg'}></StyledRescourceIcon>
           <StyledRescourceParagraphNumber>{Math.floor(currentRes.value)}</StyledRescourceParagraphNumber>
         </StyledRescourceSection>
       ))}
@@ -48,7 +35,6 @@ const StyledRescourceParagraphNumber = styled.p`
 `;
 
 const StyledRescourceIcon = styled.img`
-  padding: auto;
   width: 34px;
   height: 34px;
 `;
