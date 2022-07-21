5abf86f27bd81875914d77b0f9675f9ef73c0276
Thomas Smulski <info@makenci.de>
2022-07-21 16:26:10 +0200

    Format code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { ResourcesOverview } from "./components/resources/ResourcesOverview";
+import {useState} from 'react';
+import {ResourcesOverview} from './components/resources/ResourcesOverview';
 
 const GameSpeed = 1000;
 
@@ -12,57 +12,46 @@ const StartResourceEnergy = 0;
 const BasicProductionMoney = 20;
 const BasicProductionIron = 10;
 
-
-
-
-
-
-function productionMoney(currentLevel){
-  return Math.floor((30 * currentLevel *  Math.pow(currentLevel, 1.1) + BasicProductionMoney) * GameSpeed );
+function productionMoney(currentLevel) {
+  return Math.floor((30 * currentLevel * Math.pow(currentLevel, 1.1) + BasicProductionMoney) * GameSpeed);
 }
 
-function productionIron(currentLevel){
-  return Math.floor((20 * currentLevel *  Math.pow(currentLevel, 1.1) + BasicProductionIron) * GameSpeed );
+function productionIron(currentLevel) {
+  return Math.floor((20 * currentLevel * Math.pow(currentLevel, 1.1) + BasicProductionIron) * GameSpeed);
 }
 
-function productionFuel(currentLevel){
-  return Math.floor((10 * currentLevel *  Math.pow(currentLevel, 1.1)) * GameSpeed);
+function productionFuel(currentLevel) {
+  return Math.floor(10 * currentLevel * Math.pow(currentLevel, 1.1) * GameSpeed);
 }
-
-
 
 export default function App() {
-
   const [currentMoney, setCurrentMoney] = useState(StartResourceMoney);
   const [currentIron, setCurrentIron] = useState(StartResourceIron);
   const [currentFuel, setCurrentFuel] = useState(StartResourceFuel);
   const [currentGold, setCurrentGold] = useState(StartResourceGold);
   const [currentEnergy, setCurrentEnergy] = useState(StartResourceEnergy);
 
+  setTimeout(async () => {
+    const productionEachSecondMoney = productionMoney(0) / 3600;
+    const productionEachSecondIron = productionIron(0) / 3600;
+    const productionEachSecondFuel = productionFuel(0) / 3600;
 
-   setTimeout(async ()=>{
-
-    const productionEachSecondMoney = (productionMoney(0) / 3600)
-    const productionEachSecondIron = (productionIron(0) / 3600)
-    const productionEachSecondFuel = (productionFuel(0) / 3600)
-
-    setCurrentMoney(currentMoney + productionEachSecondMoney)
-    setCurrentIron(currentIron + productionEachSecondIron)
-    setCurrentFuel(currentFuel + productionEachSecondFuel)
-
-  },1000)
+    setCurrentMoney(currentMoney + productionEachSecondMoney);
+    setCurrentIron(currentIron + productionEachSecondIron);
+    setCurrentFuel(currentFuel + productionEachSecondFuel);
+  }, 1000);
 
   const MainResoures = [
-    {id:1,name:"Money",value:currentMoney},
-    {id:2,name:"Iron",value:currentIron},
-    {id:3,name:"Fuel",value:currentFuel},
-    {id:4,name:"Gold",value:currentGold},
-    {id:5,name:"Energy",value:currentEnergy}
-  ]
+    {id: 1, name: 'Money', value: currentMoney},
+    {id: 2, name: 'Iron', value: currentIron},
+    {id: 3, name: 'Fuel', value: currentFuel},
+    {id: 4, name: 'Gold', value: currentGold},
+    {id: 5, name: 'Energy', value: currentEnergy},
+  ];
 
-  return <>
-    <ResourcesOverview currentResource={MainResoures}/>
-
-  </>
-  ;
+  return (
+    <>
+      <ResourcesOverview currentResource={MainResoures} />
+    </>
+  );
 }
--- src/components/resources/ResourcesOverview.js
+++ src/components/resources/ResourcesOverview.js
@@ -1,5 +1,4 @@
-
-import styled from "styled-components";
+import styled from 'styled-components';
 
 import rescources_icon_money from '../../images/icons/icon_money.jpg';
 import rescources_icon_iron from '../../images/icons/icon_iron.jpg';
@@ -8,54 +7,48 @@ import rescources_icon_gold from '../../images/icons/icon_gold.jpg';
 import rescources_icon_energy from '../../images/icons/icon_energy.jpg';
 
 export const ResourcesOverview = ({currentResource}) => {
+  const IconArray = [
+    rescources_icon_money,
+    rescources_icon_iron,
+    rescources_icon_fuel,
+    rescources_icon_gold,
+    rescources_icon_energy,
+  ];
 
-    const IconArray = [rescources_icon_money,rescources_icon_iron,rescources_icon_fuel,rescources_icon_gold,rescources_icon_energy];
-
-    return (
-      <StyledRescourceContainer>
-
-        {currentResource.map((currentRes, index)=>(
+  return (
+    <StyledRescourceContainer>
+      {currentResource.map((currentRes, index) => (
         <StyledRescourceSection key={currentRes.id}>
           <StyledRescourceIcon alt={currentRes.name} src={IconArray[index]}></StyledRescourceIcon>
           <StyledRescourceParagraphNumber>{Math.floor(currentRes.value)}</StyledRescourceParagraphNumber>
-      </StyledRescourceSection>
-        ))}
-
-
-      </StyledRescourceContainer>
-    );
-  };
-
-
+        </StyledRescourceSection>
+      ))}
+    </StyledRescourceContainer>
+  );
+};
 
 const StyledRescourceContainer = styled.div`
-    margin-top: 80px;
-    border: 1px solid black;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    
-  
+  margin-top: 80px;
+  border: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
 
 const StyledRescourceSection = styled.section`
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const StyledRescourceParagraphNumber = styled.p`
-    font-size: larger;
-  
+  font-size: larger;
 `;
 
 const StyledRescourceIcon = styled.img`
   padding: auto;
   width: 34px;
   height: 34px;
- 
 `;
-
-
