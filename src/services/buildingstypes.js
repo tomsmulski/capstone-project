2aca9251c403ec1d9f20eb61bbaafa54fafc0232
Thomas Smulski <info@makenci.de>
2022-07-27 16:37:41 +0200

    add mongodb,api and first request for buildingtypes

---
+++ src/services/buildingstypes.js
@@ -0,0 +1,17 @@
+export const getBuildingsTypes = async () => {
+    try {
+      const response = await fetch(`/api/buildingstypes`);
+      if (response.ok) {
+        const buildingstypes = await response.json();
+        return buildingstypes;
+      } else {
+        throw new Error(`${response.status}`);
+      }
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  };
+  
+
+  
