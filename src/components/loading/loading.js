32e712cffe703cb2019336fff2cceb3ec0834e83
Thomas Smulski <info@makenci.de>
2022-08-03 00:29:27 +0200

    remove css file

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,22 +1,42 @@
 import styled from 'styled-components';
-import './loading.css';
-
 
 export default function Loading() {
-    
-  return (<>
-    <div className="loader"></div>
-    <StyledSpan>Loading</StyledSpan>
+  return (
+    <>
+      <StyledDiv></StyledDiv>
+      <StyledSpan>Loading</StyledSpan>
     </>
-  )
+  );
 }
 
 const StyledSpan = styled.span`
-    position: fixed;
-    top: 57%;
-    left: 50%;
-    margin-top: -120px;
-    margin-left: -30px;
-    font-size: large;
+  position: fixed;
+  top: 57%;
+  left: 50%;
+  margin-top: -120px;
+  margin-left: -30px;
+  font-size: large;
+`;
 
-`
+const StyledDiv = styled.div`
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -120px;
+  margin-left: -60px;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
